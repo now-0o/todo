3f959fe0848555827d8c6656313748a500137b87
now_0o <dlwpdn0704@gmail.com>
2023-03-03 03:03:15 +0900

    0303

--- public/script/index.js
+++ public/script/index.js
@@ -1,6 +1,9 @@
 const dateP = document.querySelector('.date');
 const dayP = document.querySelector('.day');
 const todoCount = document.querySelector('.todo-count');
+const addBtn = document.querySelector('.add-btn-container');
+const bg = document.querySelector('.bg');
+const addInput = document.querySelector('.add-input-container');
 
 const dayArray = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
 
@@ -9,3 +12,11 @@ const dt = new Date();
 dateP.innerHTML = dt.getFullYear()+'년 '+(dt.getMonth()+1)+'월 '+dt.getDate()+'일';
 dayP.innerHTML = dayArray[dt.getDay()];
 todoCount.innerHTML = '할 일 ' + ' 개 남음'
+
+addBtn.addEventListener('touchstart',()=>{
+    addBtn.classList.toggle('active');
+    console.log(addBtn.childNodes)
+    addBtn.childNodes[1].classList.toggle('active');
+    bg.classList.toggle('active')
+    addInput.classList.toggle('active')
+})
