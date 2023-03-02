d3b1fed59dd23fd9ac91f575c2d5689a4f846d2f
now_0o <dlwpdn0704@gmail.com>
2023-03-03 03:09:57 +0900

    0303

--- public/script/index.js
+++ public/script/index.js
@@ -6,12 +6,13 @@ const bg = document.querySelector('.bg');
 const addInput = document.querySelector('.add-input-container');
 
 const dayArray = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+let todoList = [];
 
 const dt = new Date();
 
 dateP.innerHTML = dt.getFullYear()+'년 '+(dt.getMonth()+1)+'월 '+dt.getDate()+'일';
 dayP.innerHTML = dayArray[dt.getDay()];
-todoCount.innerHTML = '할 일 ' + ' 개 남음'
+todoCount.innerHTML = '할 일 ' + todoList.length + ' 개 남았엉'
 
 addBtn.addEventListener('touchstart',()=>{
     addBtn.classList.toggle('active');
