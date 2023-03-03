3b100b7140ed4d679173779f8d6e2e845d9afa28
now_0o <dlwpdn0704@gmail.com>
2023-03-03 15:31:20 +0900

    0303

--- public/script/index.js
+++ public/script/index.js
@@ -3,7 +3,9 @@ const dayP = document.querySelector('.day');
 const todoCount = document.querySelector('.todo-count');
 const addBtn = document.querySelector('.add-btn-container');
 const bg = document.querySelector('.bg');
-const addInput = document.querySelector('.add-input-container');
+const addInputCon = document.querySelector('.add-input-container');
+const addInput = document.querySelector('#todo');
+const todolists = document.querySelector('.todo-lists');
 
 const dayArray = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
 let todoList = [];
@@ -16,8 +18,29 @@ todoCount.innerHTML = '할 일 ' + todoList.length + ' 개 남았엉'
 
 addBtn.addEventListener('touchstart',()=>{
     addBtn.classList.toggle('active');
-    console.log(addBtn.childNodes)
     addBtn.childNodes[1].classList.toggle('active');
-    bg.classList.toggle('active')
-    addInput.classList.toggle('active')
+    bg.classList.toggle('active');
+    addInputCon.classList.toggle('active');
+})
+
+addInput.addEventListener('keydown',(e)=>{
+    if(e.keyCode == 13){
+        e.preventDefault();
+        todoList.push(e.target.value);
+        e.target.value = ''
+        todoCount.innerHTML = '할 일 ' + todoList.length + ' 개 남았엉'
+        addBtn.classList.toggle('active');
+        addBtn.childNodes[1].classList.toggle('active');
+        bg.classList.toggle('active');
+        addInputCon.classList.toggle('active');
+        let todoDom = '';
+        const todoLi = `
+            <li class='todolist'>
+                <input type='checkbox' id='todoli'>
+                <label for='todoli'>${todoList[todoList.length-1]}</label>
+            </li>
+        `
+        todoDom += todoLi;
+        todolists.innerHTML += todoDom;
+    }
 })
